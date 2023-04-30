dd746b32ff58d139a00ac6c6976134d93ea54947
Chris Frey <christopher.r.frey1@gmail.com>
2023-04-30 11:59:06 -0600

    work non-compatible keywords

--- src/HiveQLValidator.java
+++ src/HiveQLValidator.java
@@ -19,6 +19,7 @@
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.regex.Pattern;
 import java.nio.charset.StandardCharsets;
 
 import org.apache.hadoop.hive.ql.parse.ParseDriver;
@@ -27,49 +28,58 @@ import org.apache.hadoop.hive.ql.parse.ParseException;
 public class HiveQLValidator {
     public static void main(String[] args) {
         if (args.length == 0 || args[0].length() < 5 || !args[0].substring(args[0].length()-4, args[0].length()).equals(".hql")) {
-            System.out.println("Error: An .hql filepath must be passed to this program as the first argument.");
+            System.out.println("[ERROR]: An .hql filepath must be passed to this program as the first argument.");
             System.exit(1);
         }
 
         byte[] fileBytes = null;
+        String[] splitFp = args[0].split("/");
+        String hqlFileName = splitFp[splitFp.length-1];
+        System.out.println(String.format("\n[INFO]: Opening %s for syntax validation.\n", hqlFileName));
 
         try {
             fileBytes = Files.readAllBytes(Paths.get(args[0]));
         } catch (IOException e) {
-            System.out.println("Error: Could not read .hql file. Please check the passed filepath.");
+            System.out.println("[ERROR]: Could not read .hql file. Please check the passed filepath.");
             e.printStackTrace();
             System.exit(1);
         }
 
         String queryString = new String(fileBytes, StandardCharsets.UTF_8);
         String[] queryArray = queryString.split(";");
+        Pattern pattern = Pattern.compile("\\bset\\b");
         String tmp = null;
 
         for (int i = 0; i < queryArray.length; ++i) {
             tmp = queryArray[i].toLowerCase();
-            
-            if ((tmp.contains("set") && tmp.contains("=")) || (tmp.contains("add jar") && tmp.contains(".jar"))) {
+
+            if (pattern.matcher(tmp).find() || tmp.contains("add jar") || tmp.trim().equals("")) {
                 queryArray[i] = null;
-            } 
+            }
             else {
-                queryArray[i] = tmp.replaceAll("\\$\\{.+:.+\\}\\.?", "");
+                queryArray[i] = queryArray[i].replaceAll("\\$\\{.+:.+\\}\\.?", "");
             }
         }
 
-        ParseDriver parseDriver = new ParseDriver();
+        ParseDriver parseDriver = new ParseDriver(); // This is what raises the SLF4J warning
+        System.out.println("");
+
+        int queryNum = 1;
 
         try {
             for (String query : queryArray) {
                 if (query != null) {
+                    System.out.println(String.format("[INFO]: Validating query #%d...", queryNum));
                     parseDriver.parse(query);
+                    ++queryNum;
                 }
             }
         } catch (ParseException e) {
-            System.out.println("Error: Could not parse query.");
+            System.out.println("[ERROR]: Could not parse query.");
             e.printStackTrace();
             System.exit(1);
         }
 
-        System.out.println("No issues detected with query.");
+        System.out.println(String.format("\n[INFO]: All queries in %s have passed HiveQL syntax validations", hqlFileName));
     }
 }
